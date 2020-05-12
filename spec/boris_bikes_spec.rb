b477023c379d681329d2752e654cf38ba1a26705
Rachael <rachaelewins@hotmail.com>
2020-05-12 16:49:25 +0100

    passing #release_bike test

--- spec/boris_bikes_spec.rb
+++ spec/boris_bikes_spec.rb
@@ -1,6 +1,7 @@
 require_relative  '../lib/docking_station.rb'
 describe DockingStation do
   it "Check if release_bike respond to DockingStation" do
-  expect((DockingStation.new).respond_to(release_bike)).to eq true
+    docking_station = DockingStation.new
+    expect(docking_station.respond_to?(:release_bike)).to eq true
   end
 end
