7ad8a40ac4df373f8abf2b8263e4335de8cc3952
Emanuele Pace <emanuele.10@outlook.it>
2020-05-12 16:17:02 +0100

    Add Test

--- spec/boris_bikes_spec.rb
+++ spec/boris_bikes_spec.rb
@@ -1,4 +1,6 @@
 require_relative  '../lib/docking_station.rb'
 describe DockingStation do
-
+  it "Check if release_bike respond to DockingStation" do
+  expect((DockingStation.new).respond_to(release_bike)).to eq true
+  end
 end
