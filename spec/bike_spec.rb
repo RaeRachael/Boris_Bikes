7036669156a50c474a41f5dd852e736e3b39d37c
Rachael <rachaelewins@hotmail.com>
2020-05-13 14:35:56 +0100

    test, bikes from docking stations

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -3,4 +3,7 @@ describe Bike do
   it "Check if Bike to respond_to? working?" do
     expect(Bike.new.respond_to?(:working?)).to eq(true)
   end
+  it "should be be released from DockingStation" do
+    expect(DockingStation.new.release_bike.working?).to eq(true)
+  end
 end
--- spec/boris_bikes_spec.rb
+++ spec/boris_bikes_spec.rb
@@ -3,4 +3,8 @@ describe DockingStation do
   it "Check if release_bike respond to DockingStation" do
     expect(DockingStation.new.respond_to?(:release_bike)).to eq true
   end
+
+  it "should return a bike when release_bike is called on it" do
+    expect(DockingStation.new.release_bike).to be_an_instance_of(Bike)
+  end
 end
